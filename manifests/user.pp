11bfbb66b7515534b3268d6a309037fed15e6475
Simon Croome <simon@croome.org>
2013-12-08 23:10:51 +0000

    Let user provider manage home directory
    managehome is true, so home directory should be created if it
    doesn't exist.
    
    This allows the root of the installdir to be used without causing
    a duplicate resource definition.

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,26 +1,11 @@
 class stash::user{
 
-  exec {"mkdirp-$stash::params::user_home":
-    command => "/bin/mkdir -p ${$stash::params::user_home}",
-    creates => "${stash::params::user_home}",
-  }
-
-  file {"$stash::params::user_home":
-    ensure  => directory,
-    group   => "$stash::params::group",
-    owner   => "$stash::params::user",
-    mode    => 0755,
-    require => [ Exec["mkdirp-$stash::params::user_home"], User["$stash::params::user"]],
-  }
-
   group { "$stash::params::group":
-    name    => "$stash::params::group",
     ensure  => present,
     gid     => "$stash::params::gid",
   }
 
   user { "$stash::params::user":
-    name        => "$stash::params::user",
     ensure      => present,
     home        => "$stash::params::user_home",
     managehome  => true,
@@ -28,6 +13,5 @@ class stash::user{
     gid         => "$stash::params::gid",
     comment     => "Stash Service User",
     uid         => "$stash::params::uid",
-    require     => Group["$stash::params::group"],
   }
 }
